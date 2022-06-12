c5ba9f1a7b5ac42ca964036df646f9325978cbd9
HMJ <hmj@goatpage.com>
2022-06-12 20:00:05 +0200

    peer

--- pages/peerjs.vue
+++ pages/peerjs.vue
@@ -1,35 +1,42 @@
 <script setup>
-const { $peer, $addConn,$getConn } = useNuxtApp();
+const { $connectPeer, $dataPeer, $getConn } = useNuxtApp();
 const cxninput = ref("");
 const msginput = ref("");
+const selectedinfo = ref("");
 
 const infoukey = useInfoUKey();
 const selectedfollow = useSelectedFollow();
+const follows = useFollowsUKey();
+
+const selectFollow = (f) => {
+  selectFollowUkey(f.ukey, !f.selected);
+  checkConns();
+};
+
+const checkConns = () => {
+  follows.value.forEach((f) => {
+    if ($getConn(f.info) == null) {
+      $connectPeer(f.info);
+    }
+  });
+};
 
 const onSubmitMsg = () => {
-  if (typeof $getConn !== "undefined") {
-    const conn=$getConn(selectedfollow.value);
-    if(conn!=null){
-      conn.send(msginput.value);
-    }
+  checkConns();
+  console.log("submitMsg");
+  if (typeof $dataPeer !== "undefined") {
+    useFollowsUKey().value.forEach((f) => {
+      if (f.selected) {
+        $dataPeer(f.info, msginput.value);
+      }
+    });
   }
 };
 
 const onSubmitCxn = () => {
   console.log("ok submit", cxninput.value);
-
-  if (typeof $peer !== "undefined") {
-    console.log("PEER READY");
-    var conn = $peer.connect(cxninput.value);
-    conn.on("open", function () {
-      console.log("conn", conn);
-       $addConn(conn.peer,conn)
-      conn.send("Hello from Sender");
-    });
-    conn.on("data", function (data) {
-      console.log("Conn", conn, "Peer data: ", data);
-      addPeerMessage(conn.peer,data)
-    });
+  if (typeof $connectPeer !== "undefined") {
+    $connectPeer(cxninput.value);
   }
 };
 </script>
@@ -37,13 +44,24 @@ const onSubmitCxn = () => {
 <template>
   <i-container>
     <i-row>
-      <i-column>peerjs {{ infoukey }} to {{ selectedfollow }}</i-column>
+      <i-column>peerjs {{ infoukey }} to {{ selectedinfo }}</i-column>
     </i-row>
     <i-row>
-      <i-column><PMsgList /></i-column>
+      <i-column xs="4"><FollowsList @onSelect="selectFollow" /></i-column>
+      <i-column xs="4"><PMsgList /></i-column>
+      <i-column xs="4">{{ follows }}</i-column>
     </i-row>
     <i-row>
-      <i-column xs="6">
+      <i-column xs="4"
+        ><i-button
+          type="submit"
+          color="primary"
+          class="_margin-left:1"
+          @click="checkConns"
+          >CHECK</i-button
+        ></i-column
+      >
+      <i-column xs="4">
         <i-form @submit="onSubmitCxn" size="md">
           <i-form-group inline>
             <i-form-label>CXN</i-form-label>
@@ -57,21 +75,21 @@ const onSubmitCxn = () => {
             </i-button>
           </i-form-group>
         </i-form></i-column
-      >
-      <i-form @submit="onSubmitMsg" size="md">
-        <i-form-group inline>
-          <i-form-label>MSG</i-form-label>
-          <i-input
-            name="msginput"
-            v-model="msginput"
-            placeholder="Type Msg ..."
-          />
-          <i-button type="submit" color="primary" class="_margin-left:1">
-            Send
-          </i-button>
-        </i-form-group>
-      </i-form>
-      <i-column xs="6"> </i-column>
+      ><i-column xs="4">
+        <i-form @submit="onSubmitMsg" size="md">
+          <i-form-group inline>
+            <i-form-label>MSG</i-form-label>
+            <i-input
+              name="msginput"
+              v-model="msginput"
+              placeholder="Type Msg ..."
+            />
+            <i-button type="submit" color="primary" class="_margin-left:1">
+              Send
+            </i-button>
+          </i-form-group>
+        </i-form>
+      </i-column>
     </i-row>
   </i-container>
 </template>
